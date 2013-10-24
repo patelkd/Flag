8f02b4d1a46f4656ec45b1de03eb5a842ee10c49
patelkd <karishma1126@yahoo.com>
2013-10-23 23:01:47 -0600

    Create AmericanFlag.java
    Added american flag file

---
+++ AmericanFlag.java
@@ -0,0 +1,41 @@
+-public class AmericanFlag {
+-
+-  private Rectangle stripe;
+-  private int width;
+-  private int height;
+-  private Rectangle union;
+-  
+-
+-  //constuction
+-  public AmericanFlag(int height, int width) {
+-    setHeight(height);
+-    setWidth(width);
+-    draw();
+-  }
+-
+-  public void setWidth(int width) {
+-    if (width > 0) {
+-      this.width = width;
+-    }
+-  }
+-  public void setLength(int height) {
+-    if (height>0) {
+-      this.height = height;
+-    }
+-  }
+-
+-
+-  public void draw(Graphics g) {
+-    draw.drawLongRectangle();
+-    draw.drawUnion();
+-  }
+-
+-  public static void main(String[] args) {
+-    Draw draw = new draw();
+-  }
+-  
+-}
+-
+-//stripe.draw
+-// one fill rect
+-// each object should be able to draw itself
