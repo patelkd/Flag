5db5602af0c587cecf9e2075e9d72adc1a40445f
patelkd <karishma1126@yahoo.com>
2013-10-28 16:56:37 -0600

    rectangle updates

--- Rectangle.java
+++ Rectangle.java
@@ -1,18 +1,41 @@
 import java.awt.*;
-import java.applet.*;
 
-public class Rectangle  {
 
-	private int width;
-	private int heigth;
+public class Rectangle {
+	private Point topLeft;
+	private int width, height;
+	private Color color;
 
-	public void drawLongRectangle(Graphics g, int x, int y) {
-		g.setColor(Color.red);
-		g.fillRect(0,0,width,height);
+
+	public Rectangle(Point topLeft, int width, int height, Color color) {
+		setTopLeft(topLeft);
+		setWidth(width);
+		setHeight(height);
+		setColor(color);
 	}
 
-	public void	drawUnion(Graphics g, int x, int y) {
-		g.setColor(Color.blue);
-		g.fillRect(0,0,width, height);
+	public void setTopLeft(Point topLeft) {
+		this.topLeft = topLeft;
+	}
+
+	public void setWidth(int width) {
+		if (width > 0) {
+		this.width = width;
+		}
+	}
+
+	public void setHeight(int height) {
+		if (height > 0) {
+		this.height = height;
+		}
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	public void draw(Graphics g){
+		g.setColor(this.color);
+		g.fillRect(this.topLeft.x(), this.topLeft.y(), this.width, this.height);
 	}
 }
