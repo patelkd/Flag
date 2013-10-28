72c69c03cab46c2c679d4c120c443e694a4f05a4
patelkd <karishma1126@yahoo.com>
2013-10-28 16:52:52 -0600

    Changed scale

--- Flag.java
+++ Flag.java
@@ -2,15 +2,15 @@ import java.awt.*;
 import java.applet.*;
 
 public class Flag extends Applet {
-	
-	private AmericanFlag flag;
+        
+        private AmericanFlag flag;
 
-	public void init() {
-		flag = new AmericanFlag(50, 50, 1.0);
-	}
+        public void init() {
+                flag = new AmericanFlag(50, 50, 5);
+        }
 
-	public void paint(Graphics g) {
-		flag.draw(g);
-	}
+        public void paint(Graphics g) {
+                flag.draw(g);
+        }
 
 }
