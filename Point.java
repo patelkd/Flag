843d57f6b05f070da87d27c6df5ced0208ab5dcf
patelkd <karishma1126@yahoo.com>
2013-10-28 16:53:51 -0600

    Point

---
+++ Point.java
@@ -0,0 +1,27 @@
+public class Point {
+	private int x;
+	private int y;
+
+	public Point(int x, int y) {
+		setX(x);
+		setY(y);
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void setY(int y) {
+		this.y=y;
+	}
+
+
+	public int x() {
+		return x;
+	}
+
+	public int y() {
+		return y;
+	}
+	
+}
